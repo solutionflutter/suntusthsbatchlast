65ed56dc4b56241a661b6c924997babb310f54bd
solutionflutter <solutionflutter@gmail.com>
2021-12-05 15:06:07 +0600

    initial commit

---
+++ android/app/src/main/kotlin/com/example/suntusthsbatchlast/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.suntusthsbatchlast
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Registration_Screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({Key key}) : super(key: key);
+
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
